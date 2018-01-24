383140dfa92ec8655f47df0be2287195f6f192b0
Jared Clemmensen and Russell Hofvendahl <russell@>
2018-01-24 11:44:54 -0800

    add basic roll animation and dice face pngs

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,10 @@
 
 $(document).ready(function() {
   $("button#roll").click(function() {
+    
+    $(".die").addClass("dieanimation");
     var rand = Math.floor(Math.random() * 6);
     alert(rand);
+
   });
 });
