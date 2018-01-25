1407e4d284cadf6703d149661c7aeba0e1481105
Jared Clemmensen and Russell Hofvendahl <russell@>
2018-01-24 16:09:54 -0800

    add bounce animation

--- js/scripts.js
+++ js/scripts.js
@@ -5,21 +5,21 @@ function Die() {
     var rX=0;
     var rY=0;
     var rZ=0;
-    var startValue = this.value;
+    //var startValue = this.value;
     this.value = Math.ceil(Math.random() * 6);
 
     //from value to six
-    if (startValue === 1) {
-      rY += 180;
-    } else if (startValue === 2) {
-      rY += 90;
-    } else if (startValue === 3) {
-      rX += 90;
-    } else if (startValue === 4) {
-      rY += -90;
-    } else if (startValue === 5) {
-      rX += -90;
-    }
+    // if (startValue === 1) {
+    //   rY += 180;
+    // } else if (startValue === 2) {
+    //   rY += 90;
+    // } else if (startValue === 3) {
+    //   rX += 90;
+    // } else if (startValue === 4) {
+    //   rY += -90;
+    // } else if (startValue === 5) {
+    //   rX += -90;
+    // }
 
     //from six to value
     if (this.value === 1) {
@@ -36,16 +36,24 @@ function Die() {
 
     document.documentElement.style.setProperty('--rX', rX + 360);
     document.documentElement.style.setProperty('--rY', rY + 720);
-    console.log(startValue, this.value, rX, rY);
+    console.log(this.value, rX, rY);
   }
 };
 
 $(document).ready(function() {
-  var dieOne = new Die();
+  var die1 = new Die();
 
-  $("button#roll").click(function() {
-    dieOne.setKeyframe();
-    $("#die").addClass("dieAnimation");
+  //$("#die1").removeClass("dieAnimation");
+  die1.setKeyframe();
+  //$("#die1").css("animation-play-state", "running");
+  $("#die1").addClass("dieAnimation");
+  setTimeout(function() {
+    $("#die1").css("animation-play-state", "paused");
+    $("#space").css("animation-play-state", "paused");
 
+  }, 2970);
+
+  $("#die1").click(function() {
+    location.reload();
   });
 });
